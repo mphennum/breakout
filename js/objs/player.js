03b50b49bb226e18d903846ce38ce7bd42f1009a
Max Hennum <mphennum@gmail.com>
2017-02-19 20:34:22 +0000

    starter player

---
+++ js/objs/player.js
@@ -0,0 +1,15 @@
+(function(game) {
+
+'use strict';
+
+var Objs = game.Objs = game.Objs || {};
+
+var Player = Objs.Player = function(opts) {
+
+}; // constructor
+
+Player.prototype.update = function(elapsed) {
+
+}; // update
+
+})(window.game);
