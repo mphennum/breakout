4841f87378af16c7e6633aed56dc84706b6fb81e
Max Hennum <mphennum@gmail.com>
2017-02-20 23:48:41 +0000

    light load and init

--- js/obj/light.js
+++ js/obj/light.js
@@ -2,28 +2,47 @@
 
 'use strict';
 
-var Obj = game.Obj;
+var Obj = game.Obj = game.Obj || {};
+var Light = Obj.Light = {};
 
-var Light = Obj.Light = function(opts) {
-	opts = opts || {};
+Light.__init__ = function(cb) {
+	delete Light.__init__;
 
-	Obj.call(this, opts);
+	game.load(['Renderer', 'Obj'], function() {
 
-	this.render(game.Renderer.createLight({
-		'color': opts.color,
-		'intensity': opts.intensity,
-		'shadow': opts.shadow,
-		'shadow-darkness': opts['shadow-darkness'],
-		'shadow-camera': opts['shadow-camera'],
-		'shadow-camera-top': opts['shadow-camera-top'],
-		'shadow-camera-bottom': opts['shadow-camera-bottom'],
-		'shadow-camera-left': opts['shadow-camera-left'],
-		'shadow-camera-right': opts['shadow-camera-right']
-	})); // render
+		Obj = game.Obj;
 
-	this.move(opts.x, opts.y, opts.z);
-}; // constructor
+		Light = Obj.Light = function(opts) {
+			opts = opts || {};
 
-Light.prototype = Object.create(Obj.prototype);
+			Obj.call(this, opts);
+
+			this.render(game.Renderer.createLight({
+				'color': opts.color,
+				'intensity': opts.intensity,
+				'shadow': opts.shadow,
+				'shadow-darkness': opts['shadow-darkness'],
+				'shadow-camera': opts['shadow-camera'],
+				'shadow-camera-top': opts['shadow-camera-top'],
+				'shadow-camera-bottom': opts['shadow-camera-bottom'],
+				'shadow-camera-left': opts['shadow-camera-left'],
+				'shadow-camera-right': opts['shadow-camera-right']
+			})); // render
+
+			this.move(opts.x, opts.y, opts.z);
+		}; // constructor
+
+		Light.prototype = Object.create(Obj.prototype);
+
+		if (cb) {
+			cb();
+		}
+
+	}); // load
+}; // __init__
+
+game.load('Obj', function() {
+
+}); // load
 
 })(window.game);
