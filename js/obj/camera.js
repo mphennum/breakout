ce28f48ffbcf9d47c9fbcf3ff4cba6754a47259e
Max Hennum <mphennum@gmail.com>
2017-02-21 05:26:22 +0000

    camera opt vars and render method

--- js/obj/camera.js
+++ js/obj/camera.js
@@ -10,28 +10,38 @@ Camera.__init__ = function(cb) {
 
 	game.load(['Renderer', 'Obj'], function() {
 
-		Obj = game.Obj;
+		var Renderer = game.Renderer;
+		var parent = Obj = game.Obj;
 
 		Camera = Obj.Camera = function(opts) {
 			opts = opts || {};
 
-			Obj.call(this, opts);
+			parent.call(this, opts);
 
-			this.render(game.Renderer.createCamera({
-				'fov': opts.fov,
-				'width': opts.width,
-				'height': opts.height,
-				'near': opts.near,
-				'far': opts.far
-			})); // render
+			this.width = opts.width || null;
+			this.height = opts.height || null;
 
-			this.move(opts.x, opts.y, opts.z);
+			this.fov = opts.fov || Renderer.DEFAULT_FOV;
+			this.near = opts.near || Renderer.DEFAULT_NEAR;
+			this.far = opts.far || Renderer.DEFAULT_FAR;
 		}; // constructor
 
-		Camera.prototype = Object.create(Obj.prototype);
+		Camera.prototype = Object.create(parent.prototype);
+
+		Camera.prototype.render = function() {
+			parent.prototype.render.call(this, Renderer.createCamera({
+				'fov': this.fov,
+				'width': this.width,
+				'height': this.height,
+				'near': this.near,
+				'far': this.far
+			})); // render
+		}; // render
 
 		/*Camera.prototype.update = function(elapsed) {
-
+			//this.move(0.1, 0, 0);
+			//this.move(0, 0.1, 0);
+			//this.move(0, 0, 0.1);
 		};*/ // update
 
 		if (cb) {
