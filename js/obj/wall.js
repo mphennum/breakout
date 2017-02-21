b6d98f231317fe32368942170b3c9a4d5cf48b8a
Max Hennum <mphennum@gmail.com>
2017-02-21 06:47:38 +0000

    wall extends cube

---
+++ js/obj/wall.js
@@ -0,0 +1,38 @@
+(function(game) {
+
+'use strict';
+
+var Obj = game.Obj = game.Obj || {};
+var Wall = Obj.Wall = {};
+
+Wall.__init__ = function(cb) {
+	delete Wall.__init__;
+
+	game.load(['Renderer', 'Obj', 'Obj.Cube'], function() {
+
+		var Renderer = game.Renderer;
+		Obj = game.Obj;
+
+		var parent = Obj.Cube;
+
+		Wall = Obj.Wall = function(opts) {
+			opts = opts || {};
+
+			parent.call(this, opts);
+		}; // constructor
+
+		Wall.prototype = Object.create(parent.prototype);
+
+		Wall.prototype.update = function(elapsed) {
+			//this.move(game.rand(-0.01, 0.01, true), game.rand(-0.01, 0.01, true), game.rand(-0.01, 0.01, true));
+			//this.move(0.1, 0, 0);
+		}; // update
+
+		if (cb) {
+			cb();
+		}
+
+	}); // load
+}; // __init__
+
+})(window.game);
