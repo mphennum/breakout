0b2fa0a9a5260fd7feba814f781fb0c42c433a9a
Max Hennum <mphennum@gmail.com>
2017-02-23 02:45:00 +0000

    player wider width

--- js/obj/player.js
+++ js/obj/player.js
@@ -20,7 +20,7 @@ Player.__init__ = function(cb) {
 		Player = Obj.Player = function(opts) {
 			opts = opts || {};
 
-			opts.width = opts.width || 20;
+			opts.width = opts.width || 30;
 			opts.height = opts.height || 2;
 			opts.depth = opts.depth || 2;
 
