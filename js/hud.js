494ded560833039a2c0a81813dba1abe7f51427c
Max Hennum <mphennum@gmail.com>
2017-03-12 02:09:15 +0000

    hud pause screen

--- js/hud.js
+++ js/hud.js
@@ -15,16 +15,29 @@ HUD.__init__ = function(cb) {
 	var score;
 	var $score;
 
+	var $pause;
+
 	var player;
+
+	var resize = function(event) {
+		$this.style.height = event.height;
+
+		$pause.style.top = ((event.height - $pause.offsetHeight) / 2) + 'px';
+		$pause.style.left = ((event.width - $pause.offsetWidth) / 2) + 'px';
+
+		game.log('top', (event.height - $pause.offsetHeight) / 2);
+		game.log('left', (event.width - $pause.offsetWidth) / 2);
+	}; // resize
 
 	HUD.init = function(opts) {
 		opts = opts || {};
 		$parent = opts.parent || game.elemap.$body;
 		player = opts.player;
 
+		// hud
+
 		$this = document.createElement('div');
 		$this.className = 'game-hud';
-
 		$parent.appendChild($this);
 
 		// fps
@@ -39,15 +52,33 @@ HUD.__init__ = function(cb) {
 		$score.className = 'game-hud-score';
 		$this.appendChild($score);
 
-		// resize
-		game.onresize(function(width, height) {
-			$this.style.height = height;
-		}); // onresize
+		// pause screen
 
-		// #TODO
-		// lives / balls remaining (from player)
-		// pause screen + instructions
-		// opacity / transparency
+		$pause = document.createElement('div');
+		$pause.className = 'game-hud-pause';
+		$pause.style.display = 'none';
+		$pause.innerHTML =
+			'<h1>Breakout</h1>' +
+			'<p>left: a</p>' +
+			'<p>right: d</p>' +
+			'<p>unpause: space</p>' +
+			'<p style="margin-top: 10px">credit: mph</p>'
+		;
+
+		$this.appendChild($pause);
+
+		game.listen('pause', function(event) {
+			$pause.style.display = '';
+		}); // pause
+
+		game.listen('unpause', function(event) {
+			$pause.style.display = 'none';
+		}); // unpause
+
+		// resize
+
+		game.yield(resize.bind(this, {'width': window.innerWidth, 'height': window.innerHeight}));
+		game.listen('resize', resize);
 	}; // init
 
 	HUD.update = function(elapsed) {
