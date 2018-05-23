14ee8868b28f7eda8d81c112037b75781d701ef9
Max Hennum <mphennum@gmail.com>
2018-05-22 22:24:02 -0700

    hud windows for game over and game won on game events

--- js/hud.js
+++ js/hud.js
@@ -20,29 +20,42 @@ HUD.__init__ = function(cb) {
 	var player;
 
 	var resize = function(event) {
-		$this.style.height = event.height;
-
 		for (var k in $windows) {
-			var win = $windows[k];
-			var $ele = win.$ele;
-			win.w = $ele.offsetWidth || win.w;
-			win.h = $ele.offsetHeight || win.h;
-			$ele.style.top = ((event.height - win.h) / 2) + 'px';
-			$ele.style.left = ((event.width - win.w) / 2) + 'px';
+			positionWindow($windows[k], event);
 		}
 	}; // resize
 
-	var createWindow = function(name, html) {
+	var createWindow = function(name, title, html) {
 		var win = $windows[name] = {};
 		var $ele = win.$ele = document.createElement('div');
 		$ele.className = 'game-hud-window game-hud-window-' + name;
 		$ele.style.display = 'none';
-		$ele.innerHTML = html;
+		$ele.innerHTML = '<h1>' + title + '</h1>' + html;
 
 		$this.appendChild($ele);
 
-		return $ele;
+		return win;
 	}; // createWindow
+
+	var positionWindow = function(win, event) {
+		var $ele = win.$ele;
+		win.w = $ele.offsetWidth || win.w;
+		win.h = $ele.offsetHeight || win.h;
+		$ele.style.top = ((event.height - win.h) / 2) + 'px';
+		$ele.style.left = ((event.width - win.w) / 2) + 'px';
+	}; // positionWindow
+
+	var showWindow = function(win) {
+		win.$ele.style.display = '';
+		positionWindow(win, {
+			'width': game.width,
+			'height': game.height
+		});
+	}; // showWindow
+
+	var hideWindow = function(win) {
+		win.$ele.style.display = 'none';
+	}; // hideWindow
 
 	HUD.init = function(opts) {
 		opts = opts || {};
@@ -69,8 +82,7 @@ HUD.__init__ = function(cb) {
 
 		// windows
 
-		var $pause = createWindow('pause',
-			'<h1>Breakout</h1>' +
+		var pausewin = createWindow('pause', 'Breakout',
 			'<p>left: LEFT or A</p>' +
 			'<p>right: RIGHT or D</p>' +
 			'<p>unpause: SPACE</p>' +
@@ -78,16 +90,33 @@ HUD.__init__ = function(cb) {
 		);
 
 		game.listen('pause', function(event) {
-			$pause.style.display = '';
+			showWindow(pausewin);
 		}); // pause
 
 		game.listen('unpause', function(event) {
-			$pause.style.display = 'none';
+			hideWindow(pausewin);
 		}); // unpause
+
+		var gameoverwin = createWindow('game-over', 'Game Over',
+			'<p>refresh the page to play again</p>'
+		);
+
+		var gamewonwin = createWindow('game-won', 'Congratulations',
+			'<p>You won!</p>' +
+			'<p style="margin-top: 10px">refresh the page to play again</p>'
+		);
+
+		game.listen('over', function() {
+			if (game.won) {
+				showWindow(gamewonwin);
+			} else { // lost
+				showWindow(gameoverwin);
+			}
+		});
 
 		// resize
 
-		game.yield(resize.bind(this, {'width': window.innerWidth, 'height': window.innerHeight}));
+		game.yield(resize.bind(this, {'width': game.width, 'height': game.height}));
 		game.listen('resize', resize);
 	}; // init
 
